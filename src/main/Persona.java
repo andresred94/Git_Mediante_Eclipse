88717792e5fb396dfae1f653efb2179af5ef4378
andresred94 <bravocastro.andresmartin@gmail.com>
2024-04-11 12:34:54 +0200

    Primer commit de clase Persona

--- src/main/Persona.java
+++ src/main/Persona.java
@@ -4,6 +4,9 @@ public class Persona {
 	private String nombre ;
 	private String apellido;
 	private int edad;
+	
+	//Primer commit de clase Persona
+
 
 	public Persona(String nombre, String apellido, int edad) {
 		super();
