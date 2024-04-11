8d42efa5cef9581ed0f5922f2d3a1184e4359705
andresred94 <bravocastro.andresmartin@gmail.com>
2024-04-11 12:00:12 +0200

    Primer commit para añadir el proyecto al repositorio en remoto

---
+++ src/main/ExeProgram.java
@@ -0,0 +1,9 @@
+package main;
+
+public class ExeProgram {
+	public static void main(String[] args) {
+		Estudiante e1 = new Estudiante("Andres","Bravo", 29,123,9.945f);
+		e1.mostrarDatos();
+
+	}
+}
